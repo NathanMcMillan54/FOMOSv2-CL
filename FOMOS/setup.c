a1c965b430d8a1e6193c57a2bc565ce78b9cbd58
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-01 09:29:03 -0400

    Started working on setup file.

---
+++ FOMOS/setup.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "include/FOMOS/countTime.h"
+
+int startSetup() {
+    return 0;
+}
+
+int startFomos() {
+    return 0;
+}
+
+int restart() {
+    return 0;
+}
+
+int main() {
+    int setup == 1;
+
+    if (setup == 1) {
+        startSetup();
+    } else if (setup == 0) {
+        startFomos();
+    } else {
+        printf("\a");
+        printf("There was a problem \n");
+        printf("Restarting your device now \n");
+        restart();
+    }
+
+    return 0;
+}
