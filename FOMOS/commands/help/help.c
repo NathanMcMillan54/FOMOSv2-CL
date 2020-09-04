7531a363adf1197bd829f203d2026fe4c4aecdc6
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-04 18:18:42 -0400

    Added options to help.c, github.com/NathanMcMillan54/FMOE/openFile.c will pretty much be how the files will open.

--- FOMOS/commands/help/help.c
+++ FOMOS/commands/help/help.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
 int main() {
+    int num
     printf("Help\n");
+    printf("______________________________________\n");
+    printf("| 1) About FOMOS   2) About FMOE     |\n");
+    printf("| 3) About restart 4) About shutdown |\n");
+    printf("--------------------------------------\n");
+    printf("Enter a number \n");
+    scanf("%d", &num);
+
+    switch (num) {
+        case 1:
+            // open FOMOS.txt
+            break;
+        case 2:
+            // open FMOE.txt
+            break;
+        case 3:
+            // open restart.txt
+            break;
+        case 4:
+            // open shutdown.txt
+            break;
+        default:
+            printf("NaN \n");
+    }
+
     return 0;
 }
