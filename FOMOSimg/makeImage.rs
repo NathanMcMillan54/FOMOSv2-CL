6a923862e179c823146b147f23765e6fc0b7ede1
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-07 10:45:29 -0400

    FOMOSimg/makeImage.rs wasn't making FOMOS into a .img, it now does that.

--- FOMOSimg/makeImage.rs
+++ FOMOSimg/makeImage.rs
@@ -2,7 +2,19 @@ use std::process::Command;
 
 fn main() {
     print!("Making FOMOS image... \n");
-    Command::new("dd if=/../FOMOS of=FOMOS.img bs=512 count=sectors")
-        .spawn()
-        .expect("Could not make Image");
+    // use std::process::Command;
+
+    let output = Command::new("dd")
+        .arg("if=../FOMOS/setup.c")
+        .arg("of=../FOMOS.img")
+        .arg("bs=512")
+        .arg("count=1")
+        .output()
+        .expect("failed to execute process");
+
+    println!("status: {}", output.status);
+    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+    assert!(output.status.success());
 }
