da0530240391d46f33eaa5e1cada94f05eeb50a2
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-06 23:47:40 -0400

    Added stuff to get boot working for Rust.

---
+++ boot/src/bsp.rs
@@ -0,0 +1,5 @@
+#[cfg(any(feature = "bsp_rpi3", feature = "bsp_rpi4"))]
+mod raspberrypi;
+
+#[cfg(any(feature = "bsp_rpi3", feature = "bsp_rpi4"))]
+pub use raspberrypi::*;
---
+++ boot/src/print.rs
@@ -0,0 +1,24 @@
+use crate::{bsp, console};
+use core::fmt;
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use console::interface::Write;
+
+    bsp::console::console().write_fmt(args).unwrap();
+}
+
+/// Prints without a newline.
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::print::_print(format_args!($($arg)*)));
+}
+
+/// Prints with a newline.
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ({
+        $crate::print::_print(format_args_nl!($($arg)*));
+    })
+}
