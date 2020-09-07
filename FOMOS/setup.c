d493811fa9bb7c026b227ce435c20f3656a9b104
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-07 11:55:14 -0400

    FOMOS restarts after setup now.

--- FOMOS/setup.c
+++ FOMOS/setup.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include "include/FOMOS/commands/power.h"
 #include "include/FOMOS/user/makeUser.h"
 #include "include/FOMOS/user/makeUserName.h"
 
@@ -10,12 +11,13 @@ int startSetup() {
     // make the user name for usrNam command
     make_user_name();
     sleep(1);
-    printf("Starting FOMOS 2/2 Finished \n");
-    system("./main");
     return 0;
 }
 
 void main(void) {
     startSetup();
+    printf("Starting FOMOS 2/2 Finished \n");
+    printf("Restarting to apply changes... \n");
+    restart();
     return 0;
 }
