91f5fac0ef16219ed131bbab01fbf71e779cd164
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-11 14:17:33 -0400

    Started making input for Fopen work.

--- FOMOS/commands/Fopen/Fopen.rs
+++ FOMOS/commands/Fopen/Fopen.rs
@@ -1,16 +1,22 @@
 use std::fs::File;
+use std::io;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let filename = "test.txt";
-    // Open the file in read-only mode (ignoring errors).
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(n) => {
+            println!("Opening file: {} \n", input);
+        }
+        Err(error) => println!("error: {}", error),
+    }
+    let filename = input;
+    // Read only
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
-    // Read the file line by line using the lines() iterator from std::io::BufRead.
     for (index, line) in reader.lines().enumerate() {
-        let line = line.unwrap(); // Ignore errors.
-        // Show the line and its number.
+        let line = line.unwrap();
         println!("{}", line);
     }
 }
