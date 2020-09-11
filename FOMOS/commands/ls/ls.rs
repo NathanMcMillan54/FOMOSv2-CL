0dd8786480dc0559d701b15033d45720504960ce
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-11 13:52:15 -0400

    Until there is a way of making input work for ls command it will read the error file for ls.

--- FOMOS/commands/ls/ls.rs
+++ FOMOS/commands/ls/ls.rs
@@ -1,17 +1,9 @@
 use std::fs;
-use std::io;
 
-fn main() -> std::io::Result<()> {
-    let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(n) => {
-            let paths = fs::read_dir(input).unwrap();
+fn main() {
+    let paths = fs::read_dir("commands/lsFail").unwrap();
 
-            for path in paths {
-                println!("{}", path.unwrap().path().display())
-            }
-            Ok(())
-        }
-        Err(error) => println!("error: {}", error),
+    for path in paths {
+        println!("{}", path.unwrap().path().display())
     }
 }
