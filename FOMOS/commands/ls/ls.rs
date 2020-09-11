ccdb6f4d55b8f7d922c75736478d979d59611a8b
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-10 23:33:57 -0400

    To make ls work made it similair to how mkdir works.

--- FOMOS/commands/ls/ls.rs
+++ FOMOS/commands/ls/ls.rs
@@ -1,9 +1,17 @@
 use std::fs;
+use std::io;
 
-fn main() {
-    let paths = fs::read_dir("../lsFail").unwrap();
+fn main() -> std::io::Result<()> {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(n) => {
+            let paths = fs::read_dir(input).unwrap();
 
-    for path in paths {
-        println!("{}", path.unwrap().path().display())
+            for path in paths {
+                println!("{}", path.unwrap().path().display())
+            }
+            Ok(())
+        }
+        Err(error) => println!("error: {}", error),
     }
 }
