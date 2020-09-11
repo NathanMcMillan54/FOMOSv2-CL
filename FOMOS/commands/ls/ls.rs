1e19947d2341ee06f3341e8d150904adfce69978
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-10 23:22:54 -0400

    For now ls will just open a file that says something went wrong until input directory name works.

--- FOMOS/commands/ls/ls.rs
+++ FOMOS/commands/ls/ls.rs
@@ -1,15 +1,7 @@
 use std::fs;
-use std::io;
 
 fn main() {
-    let mut dir_name = String::new();
-    match io::stdin().read_line(&mut dir_name) {
-        Ok(n) => {
-            println!("You entered: {} \n", dir_name);
-        }
-        Err(error) => println!("error: {}", error),
-    }
-    let paths = fs::read_dir(dir_name).unwrap();
+    let paths = fs::read_dir("../lsFail").unwrap();
 
     for path in paths {
         println!("{}", path.unwrap().path().display())
