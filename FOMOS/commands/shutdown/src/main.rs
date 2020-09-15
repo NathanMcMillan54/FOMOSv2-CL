cd98db003aa689547bdbe9b9658dcd038015cdda
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-15 00:00:46 -0400

    Added assembly that probably shutdown ARM CPU.

--- FOMOS/commands/shutdown/src/main.rs
+++ FOMOS/commands/shutdown/src/main.rs
@@ -4,6 +4,8 @@ use system_shutdown::shutdown;
 
 fn main() {
     match shutdown() {
+        /* get this to run system/sleep.s
+         They should only work on ARM CPU's */
         Ok(_) => println!("Shutting down..."),
         Err(error) => eprintln!("Failed to shut down: {}", error),
     }
