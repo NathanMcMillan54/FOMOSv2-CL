26fe17c55213030008abef2bcc89b2ec97a61a96
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-15 00:15:43 -0400

    Input file name for ed command works.

--- FOMOS/commands/ef/ef.rs
+++ FOMOS/commands/ef/ef.rs
@@ -1,8 +1,16 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::io;
 
 fn main() -> std::io::Result<()> {
-    let mut file = File::create("file.txt")?;
+    let mut input_file = String::new();
+    match io::stdin().read_line(&mut input_file) {
+        Ok(n) => {
+
+        }
+        Err(error) => println!("error: {}", error),
+    }
+    let mut file = File::create(input_file)?;
     file.write_all(b"ef command")?;
     Ok(())
 }
