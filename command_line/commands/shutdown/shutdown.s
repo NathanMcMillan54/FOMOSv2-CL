709221d2af73de2002456c3ecc80e0fc952fc998
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-19 15:35:47 -0400

    Added a print to shutdown.s.

--- command_line/commands/shutdown/shutdown.s
+++ command_line/commands/shutdown/shutdown.s
@@ -1,10 +1,21 @@
 // put ARM to sleep mode
 // this puts all the cores to sleep (should)
-global _start_sleep
+.syntax unified
+.global main
 
-_start_sleep:
+main:
+      push    {ip, lr}             ; print shutdown
+      ldr     r0, =printShutdown
+      bl      printf
+      mov     r0, #0    @ Return 0.
+      pop     {ip, pc}
+
       MRC   p15, 0, r0, c0, c0, 5  ; Read MPIDR
       ANDS  r0, r0, #3             ; Mask to leave CPU ID
     loop
       WFINE                        ; If not CPU 0, go into WFI in an endless loop
       BNE   loop
+
+
+printShutdown:
+    .asciz "Shutting down... \n"
