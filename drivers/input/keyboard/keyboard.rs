2045b0df70d775b32c9a0c2f15298be609d601a2
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-19 09:32:23 -0400

    Going back to the keyboard drivers that I was trying to write, I think the ones I copied only work on Linux.

---
+++ drivers/input/keyboard/keyboard.rs
@@ -0,0 +1,10 @@
+// make support for US QWERTY keyboard first
+fn look_for_event() {
+    // add stuff to look for key events
+}
+
+fn main() {
+    loop {
+        look_for_event();
+    }
+}
--- drivers/input/keyboard/src/debug_handlers.rs
+++
@@ -1,1 +0,0 @@
-pub use crate::handlers::debug_handlers::{TranslationHelper, DebugStream};
