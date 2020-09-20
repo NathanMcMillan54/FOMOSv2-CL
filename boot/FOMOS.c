dde35a0e0f3ee134088276ab5f0611e37039a234
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-20 15:00:15 -0400

    Probably don't need stdio.h.

--- boot/FOMOS.c
+++ boot/FOMOS.c
@@ -1,11 +1,7 @@
-#include "../include/stdio.h"
-
 // _start() is form src/start.s
 extern int _start();
 
 int main() {
-    printf("Starting FOMOS... \n");
-
     // this is actually fomos
     _start();
     return 0;
