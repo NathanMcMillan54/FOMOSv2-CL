25f808f2037f4dfee418ce0a20324788d28b2af7
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-23 16:42:13 -0400

    I think I can get shutdown working in just Rust.

--- src/command_line/commands/shutdown/shutdown.rs
+++ src/command_line/commands/shutdown/shutdown.rs
@@ -1,9 +1,39 @@
-extern "C" {
-    fn sleep() -> u64;
+pub fn shutdown_arm() {
+    global_asm!(r#"
+    .global sleep
+
+sleep:
+      push    {ip, lr}             ; print shutdown
+      ldr     r0, =shMsg
+      bl      printf
+      mov     r0, #0    @ Return 0.
+      pop     {ip, pc}
+
+      MRC   p15, 0, r0, c0, c0, 5  ; Read MPIDR
+      ANDS  r0, r0, #3             ; Mask to leave CPU ID
+    loop
+      WFINE                        ; If not CPU 0, go into WFI in an endless loop
+      BNE   loop
+    "#);
+    #[no_mangle]
+    pub unsafe extern "C" fn baz() {}
 }
 
-unsafe fn shutdown() {
-    // this might be the first time echo command could be used in FOMOS
-    // echo("Shutting down... \n");
-    sleep();
+extern "C" {
+    fn msg();
+    fn sleep();
+}
+
+pub fn shutdown_msg() {
+    global_asm!("r#
+        .syntax unified
+    .global msg
+    push    {ip, lr}             ; print shutdown
+      ldr     r0, =shMsg
+      bl      printf
+      mov     r0, #0    @ Return 0.
+      pop     {ip, pc}
+
+      .asciz 'Shutting down...'
+    ")
 }
