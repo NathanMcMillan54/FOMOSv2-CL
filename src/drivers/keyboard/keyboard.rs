ac46d7de0f582065455715f87f86a3822e55b091
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-24 09:11:48 -0400

    Trying to get keyboard.rs to run keyboard.s with cc crate.

--- src/drivers/keyboard/keyboard.rs
+++ src/drivers/keyboard/keyboard.rs
@@ -1,8 +1,7 @@
 use core::ptr;
-
+// use cc;
 // make support for US QWERTY keyboard first
 
-global_asm!(r#"keyboard.s"#);
 pub(crate) fn arm_keyboard() {
     const UART0: *mut u8 = 0x0900_0000 as *mut u8;
     let out_str = b"keyboard.rs \n";
@@ -13,4 +12,17 @@ pub(crate) fn arm_keyboard() {
     }
     // run keyboard_connected.s
     // run keyboard.s
+    /* cc::Build::new()
+        .file("keyboard.s")
+        .compile("keyboard");
+
+    extern {
+        fn keyboard();
+    }
+
+    pub fn keyboard_input() {
+        unsafe {
+            keyboard();
+        }
+    } */
 }
