d71d8a167f00615366700f370ba82d805d1e781f
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-27 15:20:26 -0400

    Renamed Kyboard_connect.s to keyboard_connect.s

--- src/drivers/keyboard/keyboard.rs
+++ src/drivers/keyboard/keyboard.rs
@@ -10,41 +10,5 @@ pub(crate) fn arm_keyboard() {
         }
     }
     // run keyboard_connected.s
-    // this is keyboard.s
-    /* unsafe {
-        asm!(".text
-              .global keyboard
-
-              keyboard:
-                  sub sp, sp, #4
-                  str lr, [sp, #0]
-
-                  # get ready for input
-                  ldr r0, =prompt
-                  bl  printf
-
-                  # read the input
-                  ldr r0, =format
-                  sub sp, sp, #4
-                  mov r1, sp
-                  bl  scanf
-                  ldr r2, [sp, #0]
-                  add sp, sp, #4
-
-                  # print the input
-                  mov r1, r2
-                  bl  printf
-
-                  ldr lr, [sp, #0]
-                  add sp, sp, #4
-                  mov pc, lr
-
-                  .data
-
-                  format:
-                      .asciz "%d"
-
-                  prompt:
-                      .asciz " "");
-    } */
+    // run keyboard.s
 }
