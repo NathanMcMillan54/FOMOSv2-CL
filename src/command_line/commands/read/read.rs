76df38d50f98222d45492be131ca6b9c35885b67
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-28 16:23:58 -0400

    Working on read command.

--- src/command_line/commands/read/read.rs
+++ src/command_line/commands/read/read.rs
@@ -1,9 +1,8 @@
 use core::ptr;
 
-pub(crate) fn echo() {
-    let argument = "";
+fn read() {
     const UART0: *mut u8 = 0x0900_0000 as *mut u8;
-    let out_str = argument;
+    let out_str = "Read \n";
     for byte in out_str {
         unsafe {
             ptr::write_volatile(UART0, *byte);
--- src/drivers/keyboard/keyboard.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-void keybaord() {
-    char keyboard[50];
-    scanf("%s", keyboard);
-    printf("%s", keyboard);
-}
