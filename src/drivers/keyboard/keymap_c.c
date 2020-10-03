8e1b93e3aedbad5e4d1ae77bb09fb3f68d0814ff
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-10-03 08:24:15 -0400

    Working on keyboard drivers.

--- src/drivers/keyboard/keymap_c.c
+++ src/drivers/keyboard/keymap_c.c
@@ -2,19 +2,13 @@
 
 uint8_t led_animation_id = 0;
 
-const uint16_t keymaps[][2][3] = {
-        [0] = {
-                {1, 2, 3},
-                {4, 5, 6},
-        },
-};
+extern uint16_t keymaps[][2][3];
 
 void matrix_init_user(void) {
     led_animation_id = 1;
 }
 
-void matrix_scan_user(void) {
-}
+void matrix_scan_user(void);
 
 int main() {
     matrix_init_user();
--- src/drivers/keyboard/keymap_rust.rs
+++ src/drivers/keyboard/keymap_rust.rs
@@ -7,3 +7,16 @@
 fn my_panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
+
+#[no_mangle]
+pub extern "C" fn matrix_scan_user() {
+
+}
+
+#[no_mangle]
+static keymaps: [[[u16; 3]; 2]; 1] = [
+    [
+        [1, 2, 3],
+        [4, 5, 6],
+    ],
+];
