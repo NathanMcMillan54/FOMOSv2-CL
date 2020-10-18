9190bd35a6b299ec5aaf11c08e986d356b072209
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-10-18 15:13:14 -0400

    ARM FOMOS isn't running can't find reason why, it's not really important since nobody should reallly be using this branch.

--- arch/arm/src/panic.rs
+++ arch/arm/src/panic.rs
@@ -1,8 +1,6 @@
-use crate::commands::echo::echo::echo;
 use core::panic::PanicInfo;
 
 #[panic_handler]
 fn on_panic(_info: &PanicInfo) -> ! {
-    echo(b"Panic \n");
-    loop {  }
+    panic!("There was an error");
 }
