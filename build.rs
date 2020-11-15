95b7a72895d3d162215d15858cb0b15e6fd518f7
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-11-14 23:29:20 -0500

    Writing some documentation.

--- build.rs
+++ build.rs
@@ -1,5 +1,9 @@
 extern crate cc;
 
 fn main() {
-
+    println!("cargo:rerun-if-changed=Cargo.toml");
+    println!("cargo:rerun-if-changed=src/cpu.S");
+    cc::Build::new()
+        .file("src/cpu.S")
+        .compile("src/cpu");
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,13 @@ mod panic;
 
 extern crate arch;
 
+extern "C" {
+    fn cpuArch();
+}
+
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    unsafe { cpuArch(); }
     loop { }
 }
