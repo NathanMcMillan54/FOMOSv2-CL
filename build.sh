fc1834ef06b8607d54a12cec89fdb014696e428e
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-01-12 00:16:40 -0500

    Compiling works for arm now.

--- build.sh
+++ build.sh
@@ -8,10 +8,11 @@
 ARCH=$1
 
 echo "Compiling FOMOSv2-CL v2.3.5 for $ARCH..."
-if [ "$ARCH" = 'armv6' ]; then
-    cargo build --target=armv6-unknown-linux-gnueabihf
-elif [ "$ARCH" = 'armv7' ]; then
+if [ "$ARCH" = 'armv' ]; then
     cargo build --target=armv7-unknown-linux-gnueabihf
+    arm-linux-gnueabihf-gcc -static src/main.c target/armv7-unknown-linux-gnueabihf/debug/libFOMOSv2_CL.a -o init
+    mv init initramfs/ && cd initramfs/
+    find . | cpio -o -H newc | gzip > ../FOMOSv2-CL_v2_3_5.cpio.gz
 elif [ "$ARCH" = 'x86' ]; then
     cargo build --target=x86_64-unknown-linux-gnu
     gcc -static src/main.c target/x86_64-unknown-linux-gnu/debug/libFOMOSv2_CL.a -o init
