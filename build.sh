0f6afaa7f8d68c36dda84a621268eda0c8cb3be6
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-01-13 12:24:38 -0500

    FOMOS compiles for armv6 now.

--- build.sh
+++ build.sh
@@ -8,7 +8,12 @@
 ARCH=$1
 
 echo "Compiling FOMOSv2-CL v2.3.5 for $ARCH..."
-if [ "$ARCH" = 'arm' ]; then
+if [ "$ARCH" = 'armv6' ]; then
+    cargo build --target=arm-unknown-linux-gnueabihf
+    arm-linux-gnueabihf-gcc -static src/main.c target/arm-unknown-linux-gnueabihf/debug/libFOMOSv2_CL.a -o init
+    mv init initramfs/ && cd initramfs/
+    find . | cpio -o -H newc | gzip > ../FOMOSv2-CL_v2_3_5.cpio.gz
+elif [ "$ARCH" = 'armv7' ]; then
     cargo build --target=armv7-unknown-linux-gnueabihf
     arm-linux-gnueabihf-gcc -static src/main.c target/armv7-unknown-linux-gnueabihf/debug/libFOMOSv2_CL.a -o init
     mv init initramfs/ && cd initramfs/
