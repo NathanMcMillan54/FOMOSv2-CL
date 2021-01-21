b9e1fa0d76f608945d1af758eb1622dc2fc9fa50
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-01-20 19:00:22 -0500

    Changed how FOMOS compiles, update documentation.

--- build.sh
+++ build.sh
@@ -8,22 +8,23 @@
 ARCH=$1
 
 echo "Compiling FOMOSv2-CL v2.3.5 for $ARCH..."
+rm -rf initramfs/
 rm -rf target/
 if [ "$ARCH" = 'armv6' ]; then
     cargo build --target=arm-unknown-linux-gnueabihf
     arm-linux-gnueabihf-gcc -static src/main.c target/arm-unknown-linux-gnueabihf/debug/libFOMOSv2_CL.a -o init
     mv init initramfs/ && cd initramfs/
-    find . | cpio -o -H newc | gzip > ../FOMOSv2-CL_v2_3_5.cpio.gz
+    cp init boot/initramfs && cp init initramfs/FOMOS_initramfs
 elif [ "$ARCH" = 'armv7' ]; then
     cargo build --target=armv7-unknown-linux-gnueabihf
     arm-linux-gnueabihf-gcc -static src/main.c target/armv7-unknown-linux-gnueabihf/debug/libFOMOSv2_CL.a -o init
     mv init initramfs/ && cd initramfs/
-    find . | cpio -o -H newc | gzip > ../FOMOSv2-CL_v2_3_5.cpio.gz
+    cp init boot/initramfs && cp init initramfs/FOMOS_initramfs
 elif [ "$ARCH" = 'x86' ]; then
     cargo build --target=x86_64-unknown-linux-gnu
     x86_64-linux-gnu-gcc -static src/main.c target/x86_64-unknown-linux-gnu/debug/libFOMOSv2_CL.a -o init
     mv init initramfs/ && cd initramfs/
-    find . | cpio -o -H newc | gzip > ../FOMOSv2-CL_v2_3_5.cpio.gz
+    cp init boot/initramfs && cp init boot/FOMOS_initramfs
 else
     echo "$ARCH is invalid"
 fi
